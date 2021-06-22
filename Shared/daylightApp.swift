6e19c3384d47c1383dfdbdcf77095baa3c30fafb
hrbrmstr <bob@rud.is>
2021-06-22 11:08:02 -0400

    Initial Commit

---
+++ Shared/daylightApp.swift
@@ -0,0 +1,17 @@
+//
+//  daylightApp.swift
+//  Shared
+//
+//  Created by Bob Rudis on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct daylightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
